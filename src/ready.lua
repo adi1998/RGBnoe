295e7d89f5794662e4f6f3c97db8407409d47246
Aditya Gupta <adi1998.2010@gmail.com>
2026-01-30 20:49:38 +0530

    start thread on every map load

--- src/ready.lua
+++ src/ready.lua
@@ -2,11 +2,19 @@ Hue = 0
 
 local threadStarted = false
 
+function mod.CycleDressHue()
+    while true do
+        if game.CurrentRun and game.CurrentRun.Hero and game.CurrentRun.Hero.ObjectId and not game.MapState.BabyPolymorph then
+            game.SetThingProperty({Property = "GrannyTexture", Value = "zerp-RGBnoe/" .. tostring(Hue), DestinationId = game.CurrentRun.Hero.ObjectId})
+        end
+        Hue = Hue + 2
+        Hue = (Hue + Hue%2) % 360
+        game.wait(1/config.delay)
+    end
+end
+
 modutil.mod.Path.Wrap("SetupMap", function(base)
     game.LoadPackages({Names = {_PLUGIN.guid}})
-    if not threadStarted then
-        game.thread(mod.CycleDressHue)
-        threadStarted = true
-    end
+    game.thread(mod.CycleDressHue)
     base()
 end)
