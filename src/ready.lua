36d92f9a20d958522e4e841d1be607963872f220
Aditya Gupta <adi1998.2010@gmail.com>
2026-03-15 16:14:23 +0530

    add support for DS4/Dualsense

--- src/ready.lua
+++ src/ready.lua
@@ -1,6 +1,16 @@
 Hue = 0
-
+StartColor = {202, 105, 29, 255}
 local threadStarted = false
+
+function mod.ShiftStartColor()
+    local h, s, v = rom.ImGui.ColorConvertRGBtoHSV(StartColor[1]/255, StartColor[2]/255, StartColor[3]/255)
+    h = ((h*360 + Hue) % 360) / 360
+    v = 1
+    s = 1
+    local r, g, b = rom.ImGui.ColorConvertHSVtoRGB(h, s, v)
+    r,g,b = r*255, g*255, b*255
+    game.SetLightBarColor({ PlayerIndex = 1, Color = {r,g,b,255} })
+end
 
 function mod.CycleDressHue()
     while true do
@@ -9,6 +19,7 @@ function mod.CycleDressHue()
         end
         Hue = Hue + 2
         Hue = (Hue + Hue%2) % 360
+        mod.ShiftStartColor()
         game.wait(1/config.delay)
     end
 end
